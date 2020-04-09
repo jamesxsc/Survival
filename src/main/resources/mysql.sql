1032b29f07c357c79f7f3bbfe99805cb867a86bd
BuildTools <unconfigured@null.spigotmc.org>
2020-04-09 23:14:20 +0100

    sql prep for economy

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -12,7 +12,20 @@ CREATE TABLE IF NOT EXISTS claims (
 ) DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS unlocked_items (
-    `player` VARCHAR(36) NOT NULL,
-    `item`  VARCHAR(35) NOT NULL,
+    `player`    VARCHAR(36)     NOT NULL,
+    `item`      VARCHAR(35)     NOT NULL,
     PRIMARY KEY (player, item)
+) DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS balances (
+    `player`    VARCHAR(36)     NOT NULL,
+    `balance`   INT             NOT NULL,
+    PRIMARY KEY (player)
+) DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS transactions (
+    `payee`     VARCHAR(36)     NOT NULL,
+    `payer`     VARCHAR(36)     NOT NULL,
+    `amount`    INT             NOT NULL,
+    PRIMARY KEY (payer, payee)
 ) DEFAULT CHARSET = utf8mb4;
